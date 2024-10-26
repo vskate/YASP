0140dd92b63efa266cf60c403a6107d16ed06b0b
Bartosz Kąkol <bartosz.kakol@proton.me>
2024-10-26 12:53:38 +0200

    argparse integration and support in S100_load_config

--- build/stages/S100_load_config.py
+++ build/stages/S100_load_config.py
@@ -4,12 +4,21 @@ import yaml
 
 
 class Stage(BuildStage):
+    @staticmethod
+    def setup_args(parser: ArgumentParser):
+        parser.add_argument(
+            "-c", "--config",
+            type=str,
+            default="config.yaml",
+            help="Specify the path to the configuration file"
+        )
+
     def task(self):
-        config_path = self.build_dir_path / "config.yaml"
+        config_path = Path(self.arguments.config)
 
         if not config_path.is_file():
             log(f"Could not find a config file at:", color="red", bold=True)
-            log(config_path)
+            log(config_path.resolve())
             log("The build script cannot continue without it.", color="red")
             raise InterruptBuild
 
